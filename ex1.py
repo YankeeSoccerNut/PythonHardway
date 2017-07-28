ed7a916f40d43782b7fe7a962bd952bcb48894ae
Scott Anderson <yankeesoccernut@gmail.com>
2017-07-28 18:15:46 -0400

    Second Commit

--- ex1.py
+++ ex1.py
@@ -5,3 +5,5 @@ print "This is fun."
 print 'Yay!  Printing'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+print "adding a few lines to test Git"
+print "here's another line for Git"
